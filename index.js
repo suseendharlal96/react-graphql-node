aef644b258aa391ea9be88c8f4231dcfdb6c01c5
lssuseendharlal <lssuseendharlal@gmail.com>
2020-07-01 13:51:45 +0530

    port changed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const typeDef = require("./graphql/TypeDefs");
 const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config");
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs: typeDef,
